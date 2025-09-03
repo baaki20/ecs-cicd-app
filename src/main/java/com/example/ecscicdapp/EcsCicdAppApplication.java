34dcf8cc61c00afc329787fbfa5833b6563ef022
baaki20 <huduabdulbaakijunior20@gmail.com>
2025-09-03 14:27:50 +0000

    feat: Initial application code and multi-stage Dockerfile

--- src/main/java/com/example/ecscicdapp/EcsCicdAppApplication.java
+++ src/main/java/com/example/ecscicdapp/EcsCicdAppApplication.java
@@ -2,12 +2,18 @@ package com.example.ecscicdapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class EcsCicdAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcsCicdAppApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello from Abdul Baaki N-Nyeyam Hudu! This is the ECS CI/CD Lab.";
     }
 
 }
